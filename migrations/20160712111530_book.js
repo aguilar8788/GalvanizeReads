1e9012037e0cf17892c42fc2f708f0f40b68141c
Peter Aguilar <peter.aguilar2287@gmail.com>
2016-07-12 16:11:45 -0600

    deploy issues fixed

--- migrations/20160712111530_book.js
+++ migrations/20160712111530_book.js
@@ -2,6 +2,7 @@
 exports.up = function(knex, Promise) {
   return knex.schema.createTableIfNotExists('book', function(table) {
     table.increments();
+    table.string('genre')
     table.string('title', 255);
     table.text('description');
     table.string('image');
--- routes/index.js
+++ routes/index.js
@@ -11,15 +11,31 @@ router.get('/', function(req, res, next) {
 
 router.get('/books', function(req, res, next) {
   return Promise.all([
-    knex('book_author').select('author.first_name', 'author.last_name', 'author.image as authorImage', 'book.title', 'book.image as bookImage', 'book.description').join('book', 'book_author.book_id', 'book.id').join('author', 'book_author.author_id', 'author.id'),
+    knex('book_author').select('author.first_name', 'author.last_name', 'author.image as authorImage', 'book.title', 'book.image as bookImage', 'book.description', 'book.genre').leftJoin('book', 'book_author.book_id', 'book.id').leftJoin('author', 'book_author.author_id', 'author.id'),
   ]).then(function(data) {
-    console.log(data[0][0].author);
-  res.render('books', { author_data: data[0], book_data: data[0][0] });
+    console.log(data)
+  res.render('books', { book_data: data[0] });
   })
 });
 
 router.get('/authors', function(req, res, next) {
   res.render('authors');
 })
+
+router.get('/add', function(req, res, next) {
+  res.render('addBook');
+})
+router.post('/add', function(req, res, next) {
+      knex('book').insert(req.body).then(function(data) {
+          return knex('book').where('book.title', req.body.title).then(function(data) {
+              knex('book_author').insert({book_id: data[0].id}).then(function() {
+                  res.redirect('books');
+            })
+        })
+    })
+})
+
+
+
 
 module.exports = router;
