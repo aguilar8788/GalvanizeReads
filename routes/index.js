b012df9d72ed717d7897956c825e2d83457711ff
Peter Aguilar <peter.aguilar2287@gmail.com>
2016-07-12 17:44:03 -0600

    add edit functionality

--- routes/index.js
+++ routes/index.js
@@ -13,6 +13,7 @@ router.get('/books', function(req, res, next) {
   return Promise.all([
     knex('book_author').select('author.first_name', 'author.last_name', 'author.image as authorImage', 'book.title', 'book.image as bookImage', 'book.description', 'book.genre', 'book.id').leftJoin('book', 'book_author.book_id', 'book.id').leftJoin('author', 'book_author.author_id', 'author.id'),
   ]).then(function(data) {
+    console.log(data)
   res.render('books', { book_data: data[0] });
   })
 });
@@ -36,18 +37,30 @@ router.post('/add', function(req, res, next) {
 })
 
 router.get('/:id/delete', function(req, res, next) {
-  console.log(req.params.id)
   knex('book_author').select('author.first_name', 'author.last_name', 'author.image as authorImage', 'book.title', 'book.image as bookImage', 'book.description', 'book.genre', 'book.id').leftJoin('book', 'book_author.book_id', 'book.id').leftJoin('author', 'book_author.author_id', 'author.id').where('book.id', req.params.id).then(function(data) {
     res.render('delete', { deleteBook: data[0] });
   })
 })
 
 router.post('/delete', function(req, res, next) {
-  console.log(req.body)
   return Promise.all([
     knex('book_author').del().where('book_author.book_id', req.body.id),
     knex('book').del().where('book.id', req.body.id)
   ]).then(function() {
+  res.redirect('books')
+  })
+})
+
+router.get('/:id/edit', function(req, res, next) {
+  knex('book_author').select('author.first_name', 'author.last_name', 'author.image as authorImage', 'book.title', 'book.image as bookImage', 'book.description', 'book.genre', 'book.id').leftJoin('book', 'book_author.book_id', 'book.id').leftJoin('author', 'book_author.author_id', 'author.id').where('book.id', req.params.id).then(function(data) {
+    res.render('edit', { editBook: data[0] });
+  })
+})
+
+router.post('/edit', function(req, res, next) {
+  console.log(req.body.id)
+    return knex('book').select().update(req.body).where('book.id', req.body.id)
+  .then(function() {
   res.redirect('books')
   })
 })
